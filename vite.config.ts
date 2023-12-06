c5a9beaa2306170a4413c1a40d289ff6e056b875
Harbee-pearl <yinkaajibade97@gmail.com>
2023-12-06 15:55:50 +0100

    removed unnecessary files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
+import vercel from "vite-plugin-vercel";
+
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), vercel()],
 });
